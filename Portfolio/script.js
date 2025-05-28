bb2750fee50fa0bffc49d1f169a239d2df5cecd7
Rizwana987 <rrizwanashaik987@gmail.com>
2025-05-29 01:26:33 +0530

    added portfolio website

---
+++ Portfolio/script.js
@@ -0,0 +1,32 @@
+// Tab switch functionality
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Google Sheets Form Submission
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwDtSHBwjqL7mCr9zqxwo2bgojVmdAL8wS6-ALdL3K03rxrrL3w0rgu1lnyb9dW77HM_Q/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message Sent Successfully!";
+            setTimeout(function () {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message));
+});
